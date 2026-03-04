50b960a1f8ef8267d11369fbce9e57c197ca191c
Akshra-06 <akshraahuja06@gmail.com>
2026-03-04 18:22:11 +0530

    fix redis tls connection

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,10 @@
 const redis = require("redis");
 
 const client = redis.createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true
+  }
 });
 
 client.on("error", (err) => {
